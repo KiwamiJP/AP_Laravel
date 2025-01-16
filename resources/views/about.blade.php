9f96c3dc95ea19578fdf18bac841a9122c41e62d
KiwamiJP <heinzinaung@M1-Air.local>
2025-01-16 18:48:53 +0630

    week 4 started

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-@section('content')
-<h1>About Page</h1>
-@foreach ($data as $key => $value)
-    {{ $key." = ".$value }}
-@endforeach
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-Route::get('/',[HomeController::class,'index']);
+Route::resource('posts', HomeController::class);
+
 
 
